bfda3858b3da490678ddc38cfbc6828b71bc7fb4
Kamil Kowalski <kamil@fresha.com>
2023-04-25 21:06:45 +0200

    Scaffold users

---
+++ test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule SpandexTest.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `SpandexTest.Accounts` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        age: 42,
+        name: "some name"
+      })
+      |> SpandexTest.Accounts.create_user()
+
+    user
+  end
+end
